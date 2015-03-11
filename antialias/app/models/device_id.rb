e9d11c3a9fc7afeb2d958609d21080b6e90cbe11
Kevin Hockey <kevin.hockey@cardpool.com>
2015-03-11 14:41:13 -0700

    initial example models and schema

--- antialias/app/models/device_id.rb
+++ antialias/app/models/device_id.rb
@@ -1,2 +1,11 @@
 class DeviceId < ActiveRecord::Base
+  include Neo4j::ActiveNode
+
+  property :value
+  property :created_at
+  property :updated_at
+
+  index :value
+  index :created_at
+  index :updated_at
 end
--- antialias/app/models/email.rb
+++ antialias/app/models/email.rb
@@ -1,2 +1,11 @@
 class Email < ActiveRecord::Base
+  include Neo4j::ActiveNode
+
+  property :value
+  property :created_at
+  property :updated_at
+
+  index :value
+  index :created_at
+  index :updated_at
 end

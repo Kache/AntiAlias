7efc8dd5358dff78c4a5e3d2295b78fc48bab244
Kevin Cheng <kevin@cardpool.com>
2015-03-12 03:23:40 -0700

    update schema

--- antialias/db/schema.rb
+++ antialias/db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312003033) do
+ActiveRecord::Schema.define(version: 20150312072644) do
 
   create_table "credit_cards", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "data_points", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -30,6 +35,11 @@ ActiveRecord::Schema.define(version: 20150312003033) do
   end
 
   create_table "emails", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

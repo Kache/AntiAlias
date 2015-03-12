80268b7bfe1823ee776d6b2ed49e839ee6514259
Kevin Cheng <kevin@cardpool.com>
2015-03-11 21:00:19 -0700

    fix node validation

--- antialias/lib/anti_alias_node_methods.rb
+++ antialias/lib/anti_alias_node_methods.rb
@@ -5,9 +5,14 @@ module AntiAliasNodeMethods
   end
 
   def validate_has_unique_rels
+    if !self.persisted? then return end
+
     node_pairs = self.rels.map { |r| r.nodes.map(&:neo_id).sort }
     rels_are_unique = node_pairs.uniq == node_pairs
 
     errors.add("#{self.name} has duplicate rels") if !rels_are_unique
+  end
+
+  def strongly_connected
   end
 end
